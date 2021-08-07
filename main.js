b48b0ff144d6e8759538fa61331513e142d75f82
clementine-c137 <36664819+clementine-c137@users.noreply.github.com>
2021-08-07 14:43:36 -0300

    connected api

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-const urlKey = 'https://api.rawg.io/api/platforms?c171203ffd95417e994a2949e49ca0f8'
+const urlKey = 'https://api.rawg.io/api/platforms?c171203ffd95417e994a2949e49ca0f8';
+
+const loadRawg = async function() {
+    const catalog = await fetch(urlKey, {
+        method: 'GET',
+
+    })
+    const responseJson = await  response.json();
+    if (response.status === 200) {
+        console.log(response);
+    }
+}
+
