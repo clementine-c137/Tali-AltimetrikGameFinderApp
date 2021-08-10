08f1d28f737e84473d759fa16bd9e2ac3c6511b3
clementine-c137 <36664819+clementine-c137@users.noreply.github.com>
2021-08-10 10:14:15 -0300

    rstert

--- main.js
+++ main.js
@@ -1,13 +1,70 @@
-const urlKey = 'https://api.rawg.io/api/platforms?c171203ffd95417e994a2949e49ca0f8';
+window.onload = (event) => {
+    console.log('page is fully loaded');
+    let ul = document.querySelector('.games-list');
+    ul.innerHTML = "";
+  };
+const urlKey = "https://api.rawg.io/api/games?key=c171203ffd95417e994a2949e49ca0f8";
 
-const loadRawg = async function() {
-    const catalog = await fetch(urlKey, {
+/*const loadRawg = async function() {
+    console.log("pff");
+    const response = await fetch(urlKey, {
         method: 'GET',
 
-    })
-    const responseJson = await  response.json();
-    if (response.status === 200) {
-        console.log(response);
-    }
-}
+    }).then(response => response.json())
+    .then(data => {
+        console.log('Success:', data);
+        const catalog =  JSON.parse(data);
+        catalog.forEach(element => {
+            let ul = document.getElementsByClassName('games-list');
+            let li = document.getElementsByClassName('card-display');
+            ul.appendchild(li);
+        });
+    
+    });
+}*/
 
+/*function loadRawg () {
+     fetch(urlKey, {
+        method: 'GET',
+
+    }).then(response => response.json())
+    .then(data => {
+        console.log('Success:', data);
+        const catalog =  JSON.parse(data, Array);
+        let ul = document.getElementsByClassName('games-list')[0];
+        catalog.forEach(ul => {
+            
+            let li = document.createElement('li');
+            li.className = 'card-display';
+            ul.appendchild(li);
+        });
+    
+    });
+}*/
+let cardDisplay = document.querySelector('.card-display')
+
+
+const loadRawg = async function() {
+    console.log("pff");
+    const response = await fetch(urlKey, {
+        method: 'GET',
+
+    }).then(response => response.json())
+    .then(data => {
+        console.log('Success:', data);
+        //data = JSON.parse(data.results);
+        
+        Object.entries(data).forEach( li => {
+          
+            li = document.createElement('li');
+            li.className = cardDisplay;
+           ul.appendchild(li);
+
+       });
+   
+        }); 
+      
+        
+    
+}
+loadRawg();
